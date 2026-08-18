68e2fb49821e40a24b2454200abf4e3a086bdd93
Morlinnn <1045161469@qq.com>
2026-08-18 11:04:46 +0800

    增加了可设置 delay 的重启方法

--- functional-interfaces/src/main/java/io/github/morlinnn/interfaces/Restartable.java
+++ functional-interfaces/src/main/java/io/github/morlinnn/interfaces/Restartable.java
@@ -2,4 +2,10 @@ package io.github.morlinnn.interfaces;
 
 public interface Restartable {
     void restart();
+
+    /**
+     * stop and delay
+     * @param delayMilliSeconds delay if something should stop
+     */
+    void restart(long delayMilliSeconds);
 }
